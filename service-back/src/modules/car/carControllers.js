f80a825706ba5429c6420aa4bc42d96114d709a2
vishnugopy <vishnugoby0010@gmail.com>
2022-08-14 19:09:46 +0200

    more quick improvements

--- service-back/src/modules/car/carControllers.js
+++ service-back/src/modules/car/carControllers.js
@@ -44,6 +44,23 @@ const carController = {
       console.log(e);
     }
   },
+
+  //update car
+  updateCar: async (req, res) => {
+    try {
+      const car = await prisma.car.update({
+        where: {
+          id: req.params.id,
+        },
+        data: {
+          ...req.body,
+        },
+      });
+      res.status(200).json(car);
+    } catch (e) {
+      console.log(e);
+    }
+  },
 };
 
 export default carController;
--- service-back/src/modules/car/carRouter.mjs
+++ service-back/src/modules/car/carRouter.mjs
@@ -7,6 +7,7 @@ const carRouter = Router();
 carRouter.route(`${route}`).get(carController.getAllCar);
 carRouter.route(`${route}/:id`).get(carController.findCar);
 carRouter.route(`${route}`).post(carController.addCar);
+carRouter.route(`${route}/:id`).put(carController.updateCar);
 
 
 
